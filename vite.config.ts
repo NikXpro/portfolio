dc0643c4ee4a031664ae58fde0349fc4630df1b6
NikX <41104633+NikXpro@users.noreply.github.com>
2024-06-13 18:09:15 +0200

    chore: Update base URL in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import svgr from "vite-plugin-svgr";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
-  base: "./",
+  base: "/",
   build: {
     outDir: "build",
   },
