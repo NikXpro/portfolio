5a50ffc6017a3fca42a73f18a78da80bb35e5de0
NikX <41104633+NikXpro@users.noreply.github.com>
2024-07-03 00:48:39 +0200

    refactor: Improve Card component transitions and add optional onClick and style props
    Update the Card component's SCSS file to improve the transitions for the image and filter. Reduce the transition duration from 0.5s to 0.3s for a smoother visual effect. Additionally, make the onClick prop optional and add a style prop to allow custom styling of the card container. This refactor enhances the user experience and provides more flexibility in using the Card component.

--- src/components/Ui/Card/Card.tsx
+++ src/components/Ui/Card/Card.tsx
@@ -4,19 +4,20 @@ export type CardProps = {
   title: string;
   color: string;
   image?: string;
-  onClick: () => void; // Added callback function for click
+  onClick?: () => void; // Added callback function for click
+  style?: React.CSSProperties; // Added style property
 };
 
-export const Card = (props: CardProps) => {
+export const Card = ({ title, color, image, onClick, style }: CardProps) => {
   return (
     <>
       <div
         className="card"
-        style={{ backgroundColor: props.color }}
-        onClick={props.onClick}
+        style={{ backgroundColor: color, ...style }}
+        onClick={onClick}
       >
-        {props.image && <img src={props.image} alt={props.title} />}
-        <h2>{props.title}</h2>
+        {image && <img src={image} alt={title} />}
+        <h2>{title}</h2>
       </div>
     </>
   );
