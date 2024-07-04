bb6ad2130dea9c4c50d42326d44c90f33ef28d52
NikX <41104633+NikXpro@users.noreply.github.com>
2024-07-04 02:51:18 +0200

    feat: Add ContactForm component with styling and form structure

---
+++ src/components/Section/Contact/ContactForm.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import "./ContactForm.scss";
+
+const ContactForm: React.FC = () => {
+  return (
+    <form className="contact-form">
+      <div className="form-group">
+        <input
+          type="email"
+          id="email"
+          name="email"
+          className="form-control"
+          placeholder="Email"
+        />
+      </div>
+      <div className="form-group">
+        <input
+          type="text"
+          id="name"
+          name="name"
+          className="form-control"
+          placeholder="Name"
+        />
+      </div>
+      <div className="form-group">
+        <textarea
+          id="message"
+          name="message"
+          className="form-control textarea"
+          rows={5}
+          placeholder="Message"
+        ></textarea>
+      </div>
+      <button type="submit" className="btn btn-primary">
+        Submit
+      </button>
+    </form>
+  );
+};
+
+export default ContactForm;
