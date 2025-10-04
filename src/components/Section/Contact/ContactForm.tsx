4847de03b960b43bc430512a7e56d8ff8f473593
NikX <briffanathan781@gmail.com>
2025-10-04 23:07:54 +0200

    Change API endpoint from HTTPS to HTTP

--- src/components/Section/Contact/ContactForm.tsx
+++ src/components/Section/Contact/ContactForm.tsx
@@ -28,7 +28,7 @@ export const ContactForm: React.FC = () => {
     e.preventDefault();
 
     try {
-      const response = await fetch("https://api.portfolio.nextonlab.com/contact", {
+      const response = await fetch("http://api.portfolio.nextonlab.com/contact", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
